e666e14f244f12ca6a3f3aec127fd28b74285c26
James <jamesmcelroy813@gmail.com>
2020-08-18 13:06:08 -0500

    James Update

--- lamaga/src/Cards/SenatorCard.js
+++ lamaga/src/Cards/SenatorCard.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 class SenatorCard extends React.Component {
-    
-    render(){
-        return(
-            <div></div>
-        )
-    }
+  render() {
+    let { name, party, candidateURL } = this.props;
+    return (
+      <div>
+        {this.props.name}
+        <SenatorCard name={name} party={party} url={candidateURL} />
+      </div>
+    );
+  }
 }
-export default SenatorCard
+export default SenatorCard;
