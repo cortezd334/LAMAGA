11bb9435cf2bbc418ee9b461db63a47ed4e2eb5b
cortezd334 <cortezd334@gmail.com>
2020-08-17 20:04:09 -0700

    We Tried

--- lamaga/src/Containers/RepresentativeContainer.js
+++ lamaga/src/Containers/RepresentativeContainer.js
@@ -4,6 +4,7 @@ import React from 'react';
 import RepresentativeCard from '../Cards/RepresentativeCard.js'
 
 class RepresentativeContainer extends React.Component {
+
     render() {
         return (
             <div></div>
--- lamaga/src/Containers/SenatorContainer.js
+++ lamaga/src/Containers/SenatorContainer.js
@@ -4,5 +4,14 @@ import SenatorCard from '../Cards/SenatorCard.js'
 
 class SenatorContainer extends React.Component {
 
+    
+
+    render(){
+        return (
+            <div>Hello World
+                <SenatorCard />
+            </div>
+        )
+    }
 }
 export default SenatorContainer
