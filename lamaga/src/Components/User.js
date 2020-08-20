3ba0eeb2777802c3db05da2c6e804ca73943d738
cortezd334 <cortezd334@gmail.com>
2020-08-20 10:05:16 -0700

    localstorage

--- lamaga/src/Components/User.js
+++ lamaga/src/Components/User.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class User extends React.Component {
 
-    render() {
+    componentDidMount = () => {
+        fetch(`http://localhost:3000/users/${localStorage.getItem("userID")}`) 
+        .then((res) => res.json())
+        .then(console.log);
+    }
+
+    render(){
         return (
             <div>
-                
+
             </div>
         )
     }
