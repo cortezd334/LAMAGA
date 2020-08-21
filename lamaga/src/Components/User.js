a910e9492bc4555ca5484762cafd4a6efd11a508
zanhe <zhertica@gmail.com>
2020-08-20 22:04:21 -0700

    show and update complete

--- lamaga/src/Components/User.js
+++ lamaga/src/Components/User.js
@@ -6,47 +6,97 @@ class User extends React.Component {
     
     state = {
         activeUser: {
-            name: [],
-            address: [],
+            name: '',
+            username: '',
+            address: '',
+            age: '',
+            party: ''
             
 
         }
     }
 
-    
-    
-    
-    // renderUserInfo = () => {
-    //     fetch('http://localhost:3000/users')
-    //         .then((res) => res.json())
-    //         .then(console.log);
-    //   }
-
     componentDidMount = () => {
         const activeUser = localStorage.getItem("userID");
         fetch(`http://localhost:3000/users/${activeUser}`) 
         .then((res) => res.json())
-        // .then(console.log)
         .then(json => {
             this.setState({
-            activeUser:{ 
-                name: json.name,
-                address: json.address,
-               
-            }
-        })});
+                activeUser:{ 
+                    name: json.name,
+                    username: json.username,
+                    address: json.address,
+                    age: json.age,
+                    party: json.party
+                }
+            })});
+        }
+        
+        handleChange = (e) => {
+            // e.preventDefault()
+            console.log(e)
+            console.log(e.target.name.value)
+            
+            let {name, username, address, age, party} = e.target
+            let data = { name: name.value, username: username.value, address: address.value, age: age.value , party: party.value  } 
+                    
+            const activeUser = localStorage.getItem("userID");
+            fetch(`http://localhost:3000/users/${activeUser}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    activeUser:{ 
+                        name: json.name,
+                        username: json.username,
+                        address: json.address,
+                        age: json.age,
+                        party: json.party
+                    }
+                })
+            })
     }
+    // wont update? 
+
 
     render(){
+        console.log(this.state.activeUser)
+        console.log(this.state)
         
         return (
             <Card border="secondary" >
+                <form onSubmit={(e) => {this.handleChange(e)}}>
                 <Card.Header>Profile</Card.Header>
                 <Card.Body>
-                <Card.Title><b>Name:{this.state.activeUser.name}</b></Card.Title>
-                <Card.Subtitle>Address:{this.state.activeUser.address}</Card.Subtitle>
-                <Card.Subtitle>Age:{this.state.activeUser.age}</Card.Subtitle>
+
+                <Card.Title><b>Name:    {this.state.activeUser.name}</b></Card.Title>
+                <label>Update Name: </label>
+                <input type="text" name="name" value={this.state.name} /><br></br><br></br>
+
+                <Card.Subtitle><b>Current Username: {this.state.activeUser.username}</b></Card.Subtitle>
+                <label>Update Username: </label>
+                <input type="text" name="username" value={this.state.username} /><br></br><br></br>
+
+                <Card.Subtitle><b>Current Address:  {this.state.activeUser.address}</b></Card.Subtitle>
+                <label>Update Address:  </label>
+                <input type="text" name="address" value={this.state.address} /><br></br><br></br>
+
+                <Card.Subtitle><b>Current Age:  {this.state.activeUser.age}</b></Card.Subtitle>
+                <label>Update Age:  </label>
+                <input type="text" name="age" value={this.state.age} /><br></br><br></br>
+
+                <Card.Subtitle><b>Current Party:{this.state.activeUser.party}</b></Card.Subtitle>
+                <label>Update Party:</label>
+                <input type="text" name="party" value={this.state.party} />
                 </Card.Body>
+
+                <input type="submit" value="Submit"/>
+                </form>
             </Card>
         )
     }
